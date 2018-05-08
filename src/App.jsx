cb580148697bf4c3a1d460e619f6e620acfe8c80
h <mar.ha17@outlook.com>
2018-05-08 10:59:45 +0200

    create the Activity.jsx  + new card  + avatar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 import { withStyles } from 'material-ui/styles';
 
+
 /* This is the example Card component */
 import Card from './widgets/Card/Card.jsx';
 
 import News from './widgets/News/News.jsx';
 
+import Activity from './widgets/Activity/Activity.jsx';
 import './App.css';
+
 
 const styles = theme => ({
   root: {
@@ -35,6 +38,11 @@ class App extends React.Component {
         <Grid item lg={3} md={4} sm={6} xs={12}>
           <Paper className={styles.paper}>
             <Card/>
+          </Paper>
+        </Grid>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Paper className={styles.paper}>
+            <Activity/>
           </Paper>
         </Grid>
         <Grid item lg={3} md={4} sm={6} xs={12}>
