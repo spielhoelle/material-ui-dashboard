0d70b8303596cecea441a29d0263ed954771d9a6
Jan Rübl <janruebl@yahoo.com>
2018-05-08 15:13:49 +0200

    add  updating timeText

--- src/widgets/Clock/ClockWidget.jsx
+++ src/widgets/Clock/ClockWidget.jsx
@@ -2,33 +2,37 @@ import React, {Component} from "react";
 import Clock from 'react-clock';
 import Grid from 'material-ui/Grid';
 
-
 import './ClockWidget.css';
+
+
 
 class ClockWidget extends Component {
     state = {
-        date: new Date(),
-      }
+        date: new Date()
+    }
 
-      componentDidMount(){
-          setInterval(
-              () => this.setState({date: new Date()}),
-              1000
-          );
-      }
+    componentDidMount() {
+        setInterval(() => this.setState({date: new Date()}), 1000,);
 
-      render(){
-          return(
-              <Grid item  container  spacing={0}>
-              <div className="theClock">
-                  <h3>my fancy widget</h3>
-                  <Clock value={this.state.date} />
-                  
-                  <input className="analogClock" defaultValue={this.state.date}/>
-              </div>
-              </Grid>
-          );
-      }
+    }
+
+    render() {
+        return (
+            <Grid item="item" container="container" spacing={0}>
+                <div className="theClock">
+                    <h3>my fancy widget
+                    </h3>
+                    <Clock value={this.state.date}/>
+
+                    <input
+                        readOnly="readOnly"
+                        id="clockText"
+                        className="analogClock"
+                        value={this.state.date}/>
+                </div>
+            </Grid>
+        );
+    }
 }
 
 export default ClockWidget;
