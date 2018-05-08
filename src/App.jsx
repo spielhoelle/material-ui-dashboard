792f3f0caa3a8490e158431052d8baf0e9e08b7d
Sash Seurat Samson <sash.samson@gmail.com>
2018-05-08 10:07:57 +0200

    Added News files and folder in widgets

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { withStyles } from 'material-ui/styles';
 
 /* This is the example Card component */
 import Card from './widgets/Card/Card.jsx';
+
+import News from './widgets/News/News.jsx';
+
 import './App.css';
 
 const styles = theme => ({
@@ -30,6 +33,11 @@ class App extends React.Component {
         <Grid item lg={3} md={4} sm={6} xs={12}>
           <Paper className={styles.paper}>
             <Card/>
+          </Paper>
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Paper className={styles.paper}>
+            <News/>
           </Paper>
         </Grid>
       </Grid>
